3863e93dc0250259b839c85a3de99e465e3b4318
Eleksploded <pikachu9234@gmail.com>
2019-01-31 08:02:44 -0800

    MC Versions Update!

---
+++ 1.11.2/build/sources/main/java/com/eleksploded/betterbeds/Reference.java
@@ -0,0 +1,7 @@
+package com.eleksploded.betterbeds;
+
+public class Reference {
+	public static final String ModId = "betterbeds";
+	public static final String Version = "1.1";
+	public static final String Name = "The Better Beds Mod";
+}
---
+++ Changelog.md
@@ -0,0 +1,8 @@
+#Changelog
+
+##1.1
+- Remove bed placement on end portal frames
+
+##1.0
+- Initial Release
+- **Everything is a fricken bed!**
